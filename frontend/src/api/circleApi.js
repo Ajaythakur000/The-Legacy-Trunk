047522385860304644d214146913557b98b60608
Ajaythakur000 <seemathakur252525@gmail.com>
2026-03-23 01:13:22 +0530

    14.feat(circle-management): implement dashboard circle create/select/add/remove flow with admin-only member controls and populated circle APIs

---
+++ frontend/src/api/circleApi.js
@@ -0,0 +1,43 @@
+import api from './axios';
+
+/**
+ * Create new circle
+ * body: { circleName }
+ */
+export const createCircleApi = async ({ circleName }) => {
+  const response = await api.post('/circles', { circleName });
+  return response.data;
+};
+
+/**
+ * Get all circles where logged-in user is member
+ */
+export const getMyCirclesApi = async () => {
+  const response = await api.get('/circles');
+  return response.data;
+};
+
+/**
+ * Get one circle details by ID
+ */
+export const getCircleByIdApi = async (circleId) => {
+  const response = await api.get(`/circles/${circleId}`);
+  return response.data;
+};
+
+/**
+ * Add member by email
+ * body: { email }
+ */
+export const addMemberToCircleApi = async (circleId, { email }) => {
+  const response = await api.post(`/circles/${circleId}/members`, { email });
+  return response.data;
+};
+
+/**
+ * Remove member by memberId
+ */
+export const removeMemberFromCircleApi = async (circleId, memberId) => {
+  const response = await api.delete(`/circles/${circleId}/members/${memberId}`);
+  return response.data;
+};
