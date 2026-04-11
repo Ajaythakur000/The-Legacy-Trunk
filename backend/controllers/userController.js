a3265594d6d2e80afb89c6235d4b7659bc5fff12
Ajaythakur000 <seemathakur252525@gmail.com>
2026-04-11 22:35:23 +0530

    update transporter

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -15,18 +15,18 @@ import {
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  port: 587,            
+  secure: false,         
+  requireTLS: true,     
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-
   family: 4, 
-  
- 
   connectionTimeout: 10000, 
 });
+
+
 
 const sendOtpEmail = async ({ to, subject, otpHtml, otpPlain }) => {
   try {
